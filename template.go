5865cec464c2796a143e28b26548bb187daf3bbd
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2020-11-29 21:05:23 +0100

    🚨 Fix indentation

--- template.go
+++ template.go
@@ -1,19 +1,19 @@
 import("bufio")
 import("strconv")
 func contestResponse() {
-	scanner := bufio.NewScanner(os.Stdin)
-	nbLines := -1
-	result := ""
-	for scanner.Scan() {
-		line := scanner.Text()
-		if nbLines == -1 {
-		  nbLines, _ = strconv.Atoi(line)
-		} else {
-		    result = line
-		}
-		os.Stderr.WriteString(line)
-        os.Stderr.WriteString("\n")
-	}
-	os.Stderr.WriteString("##########################\n\n")
-	fmt.Println(result)
+  scanner := bufio.NewScanner(os.Stdin)
+  nbLines := -1
+  result := ""
+  for scanner.Scan() {
+    line := scanner.Text()
+    if nbLines == -1 {
+      nbLines, _ = strconv.Atoi(line)
+    } else {
+      result = line
+    }
+    os.Stderr.WriteString(line)
+    os.Stderr.WriteString("\n")
+  }
+  os.Stderr.WriteString("##########################\n\n")
+  fmt.Println(result)
 }
