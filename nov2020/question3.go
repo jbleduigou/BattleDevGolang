1bae569216b2881fd56208e35451962c2633f08a
jbleduigou <jb.leduigou@gmail.com>
2020-11-30 21:51:17 +0100

    ♻️  question3 use defer for unlocks
    Signed-off-by: jbleduigou <jb.leduigou@gmail.com>

--- nov2020/question3.go
+++ nov2020/question3.go
@@ -18,20 +18,20 @@
 		 if nbChar == -1 {
 		   nbChar, _ = strconv.Atoi(line)
 		 } else {
-			 s := strings.Fields(line)
-			 a, _ := strconv.Atoi(s[0])
-			 b, _ := strconv.Atoi(s[1])
-			 na, found := g.GetNode(a)
+			s := strings.Fields(line)
+			a, _ := strconv.Atoi(s[0])
+			b, _ := strconv.Atoi(s[1])
+			na, found := g.GetNode(a)
 			if !found {
-				 na = &Node{value : a}
-				 g.AddNode(na)
-			 }
-			 nb, found := g.GetNode(b)
-			 if !found {
-				 nb = &Node{value : b}
-				 g.AddNode(nb)
-			 }
-			 g.AddEdge(nb, na)
+				na = &Node{value : a}
+				g.AddNode(na)
+			}
+			nb, found := g.GetNode(b)
+			if !found {
+				nb = &Node{value : b}
+				g.AddNode(nb)
+			}
+			g.AddEdge(nb, na)
 		 }
 		 os.Stderr.WriteString(line)
 		 os.Stderr.WriteString("\n")
@@ -45,12 +45,12 @@
 		current = current + 1
 		table[n.level] = current
 	}
-	 result := ""
-	 for _,k := range table {
-		  result = result + " " + strconv.Itoa(k) 
-	 }
-	 os.Stderr.WriteString("##########################\n\n")
-	 fmt.Println(result)
+	result := ""
+	for _,k := range table {
+		result = result + " " + strconv.Itoa(k) 
+	}
+	os.Stderr.WriteString("##########################\n\n")
+	fmt.Println(result)
  }
  
  type Node struct {
@@ -70,8 +70,9 @@
  // AddNode adds a node to the graph
  func (g *ItemGraph) AddNode(n *Node) {
 	 g.lock.Lock()
+	 defer g.lock.Unlock()
+
 	 g.nodes = append(g.nodes, n)
-	 g.lock.Unlock()
 	 
  }
  
@@ -93,11 +94,12 @@
  // AddEdge adds an edge to the graph
  func (g *ItemGraph) AddEdge(n1, n2 *Node) {
 	 g.lock.Lock()
+	 defer g.lock.Unlock()
+
 	 if g.edges == nil {
 		 g.edges = make(map[int][]*Node)
 	 }
 	 g.edges[n1.value] = append(g.edges[n1.value], n2)
-	 g.lock.Unlock()
  }
  
  
@@ -110,32 +112,36 @@
  // New creates a new NodeQueue
  func (s *NodeQueue) New() *NodeQueue {
 	 s.lock.Lock()
+	 defer s.lock.Unlock()
+
 	 s.items = []Node{}
-	 s.lock.Unlock()
 	 return s
  }
  
  // Enqueue adds an Node to the end of the queue
  func (s *NodeQueue) Enqueue(t Node) {
 	 s.lock.Lock()
+	 defer s.lock.Unlock()
+
 	 s.items = append(s.items, t)
-	 s.lock.Unlock()
  }
  
  // Dequeue removes an Node from the start of the queue
  func (s *NodeQueue) Dequeue() *Node {
 	 s.lock.Lock()
+	 defer s.lock.Unlock()
+
 	 item := s.items[0]
 	 s.items = s.items[1:len(s.items)]
-	 s.lock.Unlock()
 	 return &item
  }
  
  // Front returns the item next in the queue, without removing it
  func (s *NodeQueue) Front() *Node {
 	 s.lock.RLock()
+	 defer s.lock.RUnlock()
+
 	 item := s.items[0]
-	 s.lock.RUnlock()
 	 return &item
  }
  
@@ -143,6 +149,7 @@
  func (s *NodeQueue) IsEmpty() bool {
 	 s.lock.RLock()
 	 defer s.lock.RUnlock()
+
 	 return len(s.items) == 0
  }
  
@@ -150,12 +157,15 @@
  func (s *NodeQueue) Size() int {
 	 s.lock.RLock()
 	 defer s.lock.RUnlock()
+
 	 return len(s.items)
  }
  
  // Traverse implements the BFS traversing algorithm
  func (g *ItemGraph) Traverse(f func(*Node)) {
 	 g.lock.RLock()
+	 defer g.lock.RUnlock()
+
 	 q := NodeQueue{}
 	 q.New()
 	 n := g.nodes[0]
@@ -181,5 +191,4 @@
 			 f(node)
 		 }
 	 }
-	 g.lock.RUnlock()
  }
